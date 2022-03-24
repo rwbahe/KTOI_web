2a08c5d513ae340bffd3d7e0757d493502075f19
Sonny <rwbahe@gmail.com>
2022-03-23 19:51:31 -0600

    production

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     $("#menu").click(function() {
         if ( $("#dropdown").css("display") == "none" ) {
             $("#dropdown").css("display", "flex");
-            $("#menu").css("background-image", "url(icons/cancel_36x36.svg)");
+            $("#menu").css("background-image", "url(icons/menu-cancel_36x36.svg)");
         }
         else {
             $("#dropdown").css("display", "none");
-            $("#menu").css("background-image", "url(icons/menu_36x36.svg)");
+            $("#menu").css("background-image", "url(icons/menu-bar_36x36.svg)");
         }
     });
 });
